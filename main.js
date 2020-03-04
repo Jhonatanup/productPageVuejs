675c0024bb0afe8d974d1dd4735403a60c4e343e
jhonatanup <jleandroweb@gmail.com>
2020-03-04 18:43:49 -0400

    event handling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var app = new Vue ({
     el: '#app',
     data: {
+        cart: 0,
         product: 'meias',
         description: 'melhores meias que você ja vue',
         details: ['80% algodão', '20% poliester', 'Gender-neutral'],
@@ -11,12 +12,26 @@ var app = new Vue ({
         variants: [
             {
                 variantId: 2234,
-                variantColor: 'verde'
+                variantColor: 'verde',
+                variantImage: './assets/vmSocks-green-onWhite.jpg'
             },
             {
                 variantId: 2235,
-                variantColor: 'azul'
+                variantColor: 'azul',
+                variantImage: './assets/vmSocks-blue-onWhite.jpg'
             }
         ],
-    }
+    },
+    methods: {
+        addToCart(){
+            this.cart +=1
+        },
+        removeToCart(){
+            if(this.cart > 0)
+                this.cart -=1
+        },
+        updateProduct(img){
+            this.image = img
+        }
+    },
 })
