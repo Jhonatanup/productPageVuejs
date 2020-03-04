66d171b39cb537d1c4ce6071fb0607f609e4394e
jhonatanup <jleandroweb@gmail.com>
2020-03-04 11:54:33 -0400

    attribute binding

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ var app = new Vue ({
     el: '#app',
     data: {
         product: 'meias',
-        description: 'melhores meias que você ja vue'
+        description: 'melhores meias que você ja vue',
+        image: './assets/vmSocks-green-onWhite.jpg',
+        altText: 'um par de meia'
     }
 })
