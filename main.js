d41682e0ea31618e92298d29ebbed4d66341a02e
jhonatanup <jleandroweb@gmail.com>
2020-03-04 14:38:57 -0400

    list rendering

--- main.js
+++ main.js
@@ -3,9 +3,20 @@ var app = new Vue ({
     data: {
         product: 'meias',
         description: 'melhores meias que você ja vue',
+        details: ['80% algodão', '20% poliester', 'Gender-neutral'],
         image: './assets/vmSocks-green-onWhite.jpg',
         altText: 'um par de meia',
         inStock: false,
         inventory: 9,
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'verde'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'azul'
+            }
+        ],
     }
 })
